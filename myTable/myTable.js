0f16e9a490cc87dc7d262734e0e32d680f676fdd
Claire Bates <clairebates1987@hotmail.co.uk>
2017-08-11 11:16:53 +0100

    Latest example updates

--- myTable/myTable.js
+++ myTable/myTable.js
@@ -1,11 +1,34 @@
 var myTable = new Vue({
-	el: '#myTable',
+	el: '#lessons',
 	data: {
+		lessonColumns: [ 'Name', 'Subject', 'No of. Slides' ]
 		lessons: [
 			{ name: 'Test lesson 1', subject: 'Maths', slides: 2 },
 			{ name: 'Test lesson 2', subject: 'English', slides: 8 },
 			{ name: 'Test lesson 3', subject: 'Science', slides: 5 }
 		],
 		searchQuery: ''
-	}
+	},
+	computed: {
+        // A computed property that holds only those lessons that match the searchQuery.
+        filteredResults: function () {
+            var lessons_array = this.lessons,
+                searchQuery = this.searchQuery;
+
+            if(!searchQuery){
+                return lessons_array;
+            }
+
+            searchQuery = searchQuery.trim().toLowerCase();
+
+            lessons_array = lessons_array.filter(function(item){
+                if(item.name.toLowerCase().indexOf(searchQuery) !== -1 || item.subject.toLowerCase().indexOf(searchQuery) !== -1){
+                    return item;
+                }
+            })
+
+            // Return an array with the filtered data.
+            return lessons_array;
+        }
+    }
 });
